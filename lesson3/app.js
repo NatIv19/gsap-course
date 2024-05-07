a8751f58932078aa82e8d9a3874c190ded523379
NatIv19 <natalya.ivanovna19@gmail.com>
2024-05-07 23:30:46 +0300

    3

---
+++ lesson3/app.js
@@ -0,0 +1,7 @@
+const titleProject = "Оценка стоимости";
+const screensValue = ["шаблонные", "с уникальным дизайном", "с анимациями"];
+const screenPrice = 1000;
+const percentage = 20;
+const fullPrice = 3500;
+const responsive = true;
+console.log(titleProject, screensValue, screenPrice, percentage, fullPrice, responsive);
