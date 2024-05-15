c9715b15e9195340ac1bd29ded92f2529efcdf37
NatIv19 <natalya.ivanovna19@gmail.com>
2024-05-15 15:56:02 +0300

    lesson6

--- lesson4/lesson4.js
+++ lesson4/lesson4.js
@@ -3,46 +3,57 @@
  let screenPrice = 1000;
  let percentage = 10;
  let fullPrice = 3500;
- let responsive = true;
+let responsive = true;
+let allServicePrices;
+let service = [];
+let service2;
+let servicePrice;
+let servicePrice2;
 // console.log(titleProject, screensValue, screenPrice, percentage, fullPrice, responsive);
 
- titleProject = prompt("Название проекта?");
-let value = prompt("Тип экрана? 1 - шаблонные, 2 - с уникальным дизайном, 3 - с анимациями");
-if (value == 1) screensValue = screensValue[0];
-else if (value == 2) screensValue = screensValue[1];
-else if (value == 3) screensValue = screensValue[2];
+const asking = function () {
+    titleProject = prompt("Название проекта?");
+    let value = prompt("Тип экрана? 1 - шаблонные, 2 - с уникальным дизайном, 3 - с анимациями");
+    if (value == 1) screensValue = screensValue[0];
+    else if (value == 2) screensValue = screensValue[1];
+    else if (value == 3) screensValue = screensValue[2];
+    value = prompt("Нужен ли респонсивный сайт? да/нет");
+    if (value == "да") responsive = true;
+    if (value == "нет") responsive = false;
+ }
 
-value = prompt("Нужен ли респонсивный сайт? да/нет");
-if (value == "да") responsive = true;
-if (value == "нет") responsive = false;
+const getAllServicePrices = function () {
+    let sum = 0;
+    for (let i = 0; i < 2; i++){
+        if (i == 0) service[i] = prompt("Какой сервис нужен?");
+        if (i>0) service[i] = prompt("Какой еще сервис нужен?");
+        servicePrice = +prompt("Сколько это будет стоить?"); 
+        while (!isNumber(servicePrice)) {
+            servicePrice = +prompt("Уточните чтоимость. Введите число. Сколько это будет стоить?");
+        }
+        sum += servicePrice;
+    }
+    return sum;
+}
 
-let service1 = prompt("Какой сервис нужен?");
-let servicePrice1 = +prompt("Сколько это будет стоить?");
-let service2 = prompt("Какой еще сервис тебе нужен?");
-let servicePrice2 = +prompt("Сколько будет стоить этот второй сервис?");
 
-fullPrice = screenPrice + servicePrice1 + servicePrice2;
-let servicePercentPrice = Math.round(fullPrice * (1 - percentage / 100));
-console.log(servicePercentPrice);
 
-if (fullPrice > 50000) console.log("Сделаем скидку 10%");
-else if (fullPrice > 20000 && fullPrice <= 50000) console.log("Сделаем скидку 5%");
-else if (fullPrice <= 20000 && fullPrice > 0) console.log("Скидка не предусмотрена");
-else if (fullPrice <= 0) console.log("Что-то пошло не так");
+// fullPrice = screenPrice + servicePrice1 + servicePrice2;
+// let servicePercentPrice = Math.round(fullPrice * (1 - percentage / 100));
+// console.log(servicePercentPrice);
+
+// if (fullPrice > 50000) console.log("Сделаем скидку 10%");
+// else if (fullPrice > 20000 && fullPrice <= 50000) console.log("Сделаем скидку 5%");
+// else if (fullPrice <= 20000 && fullPrice > 0) console.log("Скидка не предусмотрена");
+// else if (fullPrice <= 0) console.log("Что-то пошло не так");
 
 const getAllPrices = function() {
     return servicePrice1 + servicePrice2;
 }
 
-let allServicePrices = getAllPrices();
-console.log("allServicePrices=", allServicePrices);
-
 function getFullPrice() {
     return allServicePrices + screenPrice;
 }
-
-fullPrice = getFullPrice();
-console.log("fullPrice=", fullPrice);
 
 function getTitle() {
     titleProject = titleProject[0].toUpperCase() + titleProject.slice(1).toLowerCase();
@@ -53,8 +64,7 @@ console.log("titleProject=", getTitle());
 function getServicePercentPrices() {
     return Math.round(fullPrice * (1 - percentage / 100));
 }
-servicePercentPrice = getServicePercentPrices();
-console.log("servicePercentPrice=", servicePercentPrice);
+
 
 function getRollbackMessage() {
     let discount = 0;
@@ -66,4 +76,20 @@ function getRollbackMessage() {
     console.log("Ваша скидка = ", discount, "%");
 }
 
-getRollbackMessage();
+function isNumber(text) {
+    return isFinite(text); 
+}
+
+//getRollbackMessage();
+
+
+asking();
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice();
+servicePercentPrice = getServicePercentPrices();
+getTitle();
+
+console.log("titleProject = ", titleProject);
+console.log("allServicePrices = ", allServicePrices);
+console.log("fullPrice = ", fullPrice);
+console.log("servicePercentPrice = ", servicePercentPrice);
