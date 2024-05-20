3874ae6698032199d727b6fbcb771d7825d1c590
NatIv19 <natalya.ivanovna19@gmail.com>
2024-05-20 10:27:18 +0300

    8

--- lesson8/lesson8.js
+++ lesson8/lesson8.js
@@ -1,5 +1,37 @@
 let droptitle='';
-const droplink = document.querySelector('.drop-link animate w-inline-block');
-droplink.addEventListener('click', () => {
-console.log('clicked');
-});
+const droplink = document.querySelectorAll('.drop-link.animate.w-inline-block');
+const dropimg = document.querySelectorAll('.drop-img2');
+console.log(droplink);
+console.log(dropimg);
+
+droplink.forEach((dropLink, index) => {
+    dropLink.addEventListener('click', () => {
+        console.log('clicked ' + index);
+        // dropimg[index * 2].classList.toggle('hide');
+        // dropimg[index * 2+1].classList.toggle('hide');
+    })
+})
+
+// const tabMenu = document.querySelectorAll('.tab-link');
+// const tabContent = document.querySelectorAll('.tab-pane');
+// const tabNum = document.querySelectorAll('.tab-index');
+
+// console.log(tabMenu);
+// console.log(tabContent);
+
+// tabMenu.forEach((tabLink, index) => {
+// 	tabLink.addEventListener('click', () => {
+//   	tabMenu.forEach(tab => {
+//     	tab.classList.remove('active');
+//     })
+//   	tabMenu[index].classList.add('active');
+//   	tabContent.forEach( tabItem => {
+//     	tabItem.classList.remove('active')
+//     })
+//     tabContent[index].classList.add('active');
+//   })
+// })
+
+// tabNum.forEach((item, index) => {
+// 	item.textContent = index+1;
+// })
